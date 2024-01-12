4449542909bfde21619b95ad6c58d2d74fa0f7b5
Sobhanjan66 <152434378+Sobhanjan66@users.noreply.github.com>
2024-01-12 20:37:10 +0000

    update

--- .devcontainer/01.Basics/Basics.js
+++ .devcontainer/01.Basics/Basics.js
@@ -1,1 +1,15 @@
 console.log("Sobhanjan")
+const accountId = 144553
+let email = "Sobhanjan@gmail.com"
+var password = "12345"
+city = "Gangarampur"
+let state;
+
+// Can't assign & print different values under same const type variable
+
+console.log();
+console.table([accountId, email, password, city, state])
+/*
+Prefer not to use var 
+because of issue in block scope & functional scope
+*/
