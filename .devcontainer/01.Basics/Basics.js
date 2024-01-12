096a877f31b5aab8dcba99ceea351b904444d2ae
Sobhanjan66 <152434378+Sobhanjan66@users.noreply.github.com>
2024-01-12 21:51:49 +0000

    commit

--- .devcontainer/01.Basics/Basics.js
+++ .devcontainer/01.Basics/Basics.js
@@ -1,15 +1,37 @@
+//"use strict"; // treat all js code as newer version (No need to write nowadays)
+
 console.log("Sobhanjan")
 const accountId = 144553
 let email = "Sobhanjan@gmail.com"
 var password = "12345"
+let age = 22
 city = "Gangarampur"
 let state;
 
 // Can't assign & print different values under same const type variable
 
 console.log();
-console.table([accountId, email, password, city, state])
+console.table([accountId, email, password, age, city, state])
 /*
 Prefer not to use var 
 because of issue in block scope & functional scope
 */
+
+/* Documentation : MDN
+Original Standards Documentation : ECMAScript */
+
+/* Data types:
+   1.Primitive Datatypes:
+   number => 2 to power about 53
+   bigint => Extremely large value of number
+   string => ""
+   boolean => true/false
+   null => representation of empty space
+   undefined => variable assigned but has not been defined with any value
+   symbol => uniqueness
+   2. Object:
+   */
+
+console.log(typeof "Sobhanjan")
+console.log(typeof age);
+
