713c830df2d7b85a67192642d466faed697ecd91
Sobhanjan66 <152434378+Sobhanjan66@users.noreply.github.com>
2024-01-31 21:11:34 +0000

    arrays

--- JavaScript_Codes/Array.js
+++ JavaScript_Codes/Array.js
@@ -36,4 +36,31 @@ console.log("B", array);
 console.log(array.splice(1,3)); // splice cut down the selected portion from array & then print that & manipulate/change the original array
 console.log("C", array);
 
+const marvel_heros = ["Ironman","Doctor Strange","Captain America"];
+const dc_heros = ["Batman","Superman","Flash"];
+//marvel_heros.push(dc_heros);
+//console.log(marvel_heros); //[
+//     'Ironman',
+//     'Doctor Strange',
+//     'Captain America',
+//     [ 'Batman', 'Superman', 'Flash' ]
+//   ]
+//console.log(marvel_heros[3][2]);
+//marvel_heros.concat(dc_heros);
+//console.log(marvel_heros); //Same Output as push method
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
+const all_new_heros = [...marvel_heros,...dc_heros]
+console.log(all_new_heros);
 
+const another_array = [1,2,3,[4,5,6],7,[8,9,[10,11]]]
+const another = another_array.flat(Infinity)
+console.log(another);
+
+console.log(Array.isArray("Sobhanjan"));
+console.log(Array.from("Sobhanjan"));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
